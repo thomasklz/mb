5c3ce18d5a43a345c797fb9d76da56c6fb504ecd
Jean Rodriguez <jeanpierrerodriguezzambrano@gmail.com>
2023-07-11 22:38:17 -0500

    validacion imagen

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -19,11 +19,14 @@ class ImagenController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-     public $rulesImagenes=array(
+    /*   public $rulesImagenes=array(
 
         'imagen' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:10000|min:1000|dimensions:max_width=4032,max_height=4032',
-);
-    public $mensajes=array(
+); */
+    public $rulesImagenes = array(
+        'imagen' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:10000|min:1000',
+    );
+    public $mensajes = array(
 
         'imagen.required' => 'Se requiere una imagen.',
         'imagen.image' => 'Solo se permite imagenes.',
@@ -47,35 +50,34 @@ class ImagenController extends Controller
     public function store(Request $request)
     {
         //
-         $usuario=Auth::guard('sanctum')->user();
-        if($usuario->rol!=='participante'){
-            return response()->json(["error"=>"no autorizado"],403);
+        $usuario = Auth::guard('sanctum')->user();
+        if ($usuario->rol !== 'participante') {
+            return response()->json(["error" => "no autorizado"], 403);
         }
 
-        $validator= Validator::make($request->all(),$this->rulesImagenes,$this->mensajes);
-        if($validator -> fails()){
-            $messages=$validator->getMessageBag();
+        $validator = Validator::make($request->all(), $this->rulesImagenes, $this->mensajes);
+        if ($validator->fails()) {
+            $messages = $validator->getMessageBag();
             return response()->json([
-                'messages'=>$messages
-            ],500);
+                'messages' => $messages
+            ], 500);
         }
         $file = request()->file('imagen');
-        try{
-        $obj = Cloudinary::upload($file->getRealPath(),['folder'=>'AmbienteSaludable']);
-        $imagen_id = $obj->getPublicId();
-        $url = $obj->getSecurePath();
+        try {
+            $obj = Cloudinary::upload($file->getRealPath(), ['folder' => 'AmbienteSaludable']);
+            $imagen_id = $obj->getPublicId();
+            $url = $obj->getSecurePath();
 
-        $imagen = Imagen::create([
+            $imagen = Imagen::create([
 
-            'imagen_url'=>$url,
-            'id_imagen'=>$imagen_id,
+                'imagen_url' => $url,
+                'id_imagen' => $imagen_id,
 
-        ]);
-        return response()->json(['messages'=>'Se creo una la imagen con exito.','imagen'=> $imagen]);
-    } catch (\Exception $e) {
-        return response()->json(['error' => $e->getMessage()], 500);
-    }
-
+            ]);
+            return response()->json(['messages' => 'Se creo una la imagen con exito.', 'imagen' => $imagen]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 
     /**
